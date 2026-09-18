8df8328967fe9fe4d831a18fd7dc5b056231ecb8
milanstevic1992ts-gif <milanstevic1992ts@gmail.com>
2026-09-18 11:45:00 +0200

    EMS SEO v1.4.1: align test version marker

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 define( 'ABSPATH', __DIR__ . '/' );
-define( 'EMS_LOCAL_SEO_VERSION', '1.4.0-test' );
+define( 'EMS_LOCAL_SEO_VERSION', '1.4.1-test' );
 
 $GLOBALS['ems_test_can_manage'] = true;
 $GLOBALS['ems_test_nonce_calls'] = array();
